3fa4a96b7d99f810f5a523b6c790db9e893a53b4
Connor Campagna <2894621C@student.gla.ac.uk>
2026-03-05 14:41:06 +0000

    new django version

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/connorcampagna/Desktop/UNI/Year Two /WAD/Group Project/CareOrbit/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
